9682928e52908cc1d4460b93892ec7797a2c9a7a
Furkan <furkancilogluuu@gmail.com>
2025-11-16 02:53:46 +0200

    Add dynamic SEO meta tags

--- forms.py
+++ forms.py
@@ -12,18 +12,6 @@ class MultiCheckboxField(SelectMultipleField):
     option_widget = CheckboxInput()
 
 # WTForm for creating or editing a blog post
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[Optional(), URL()])
-    cover_image = FileField("Upload Cover Image", validators=[
-        Optional(),
-        FileAllowed(['jpg', 'jpeg', 'png', 'webp'], "Only image files are allowed.")
-    ])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    categories = MultiCheckboxField("Categories", coerce=int)
-    submit = SubmitField("Submit Post")
-
 class ContactForm(FlaskForm):
     # Basic fields
     name = StringField("Name", validators=[DataRequired(), Length(max=120)])
